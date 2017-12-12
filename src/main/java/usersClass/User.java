cf2790fdb1293c558a1871aa7546d9ec0f205ad4
Pierre Fournier <peiofour@gmail.com>
2017-12-12 12:45:21 +0100

    adding class User

--- src/main/java/usersClass/User.java
+++ src/main/java/usersClass/User.java
@@ -1,4 +1,27 @@
 package usersClass;
 
-public class User {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+public abstract class User {
+
+    @NotBlank
+    private String firstname;
+    @NotBlank
+    private String lastname;
+    @NotBlank
+    @Pattern(regexp = "\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}\\b")
+    private String email;
+    @NotBlank
+    private String password;
+
+
+
+
+    public User(String _firstname, String _lastname, String _email, String _password){
+        this.firstname = _firstname;
+        this.lastname = _lastname;
+        this.email = _email;
+        this.password = _password;
+    }
 }
